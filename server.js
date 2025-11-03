52543b3fd8cead364964adf3b0f7184b061cd89a
1xposed <karstenesserr@gmail.com>
2025-11-03 13:58:39 -0500

    added users routes

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 
 const app = express(); //calling this function, sets up a server
+app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res)=> {
     console.log(`here`);
-    res.send("Hello World!")
+    res.render("index", {user:"karsten"});
 }); //this function will run when someone goes to the root folder
 
+app.get('/users', (req, res)=>{
+    res.send('User List');
+
+});
+
+app.get('/users/new', (req, res)=>{
+    res.send('new user form');
+});
 
 
 
